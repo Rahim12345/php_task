8992c1e547f3f9c1767d1681e0320ca60ae60497
Rahim12345 <rahim.suleymanov94@gmail.com>
2023-02-03 15:42:16 +0400

    Task finished

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,6 @@
 use App\Http\Controllers\ShortLinkController;
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-
-
-//Route::get('generate-shorten-link', 'ShortLinkController@index');
-//Route::post('generate-shorten-link', 'ShortLinkController@store')->name('generate.shorten.link.post');
-//
-//Route::get('{code}', 'ShortLinkController@shortenLink')->name('shorten.link');
-
 Route::resource('/',ShortLinkController::class);
 
 Route::get('{code}', [ShortLinkController::class,'show'])->name('show');
